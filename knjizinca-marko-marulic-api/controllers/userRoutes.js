b452047268bdac8f32b5df6f0df0fdf29e4e4132
FedaJakic <fedja.jakic@gmail.com>
2024-01-07 12:11:49 +0100

    added API routes for user editing

---
+++ knjizinca-marko-marulic-api/controllers/userRoutes.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { User } from '../models/User.js';
+const router = express.Router();
+
+// @desc    Get user by ID
+// @route   GET /api/users/userProfile/:userId
+// @access  Public
+router.get(
+	'/userProfile/:userId',
+	asyncHandler(async (req, res) => {
+		try {
+			const { userId } = req.params;
+			const userProfile = await User.findOne({
+				where: {
+					id: userId,
+				},
+			});
+			res.status(200).json(userProfile);
+		} catch (error) {
+			res.status(500).json({ error: error.message });
+		}
+	})
+);
+
+// @desc    Update user by ID
+// @route   PUT /api/users/userProfile/:userId
+// @access  Public
+router.put(
+	'/userProfile/:userId',
+	asyncHandler(async (req, res) => {
+		const { userId } = req.params;
+		try {
+			const [updatedRows, updatedInstance] = await User.update(req.body, {
+				where: { id: userId },
+				returning: true,
+			});
+			if (!updatedRows) {
+				throw new Error('Nothing updated!');
+			}
+			res.status(200).json({
+				message: 'Successfully updated user!',
+				data: updatedInstance,
+			});
+		} catch (error) {
+			res.status(500).json({ error: error.message });
+		}
+	})
+);
+
+export default router;
