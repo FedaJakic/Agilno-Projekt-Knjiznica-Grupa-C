90a97bae2a282260e83c9806eb1dc54fe13b8173
FedaJakic <fedja.jakic@gmail.com>
2024-01-07 21:44:11 +0100

    new design

--- knjiznica-marko-marulic/src/App.js
+++ knjiznica-marko-marulic/src/App.js
@@ -1,40 +1,81 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
-import Prijava from "./pages/Prijava";
-import Registracija from "./pages/Registracija";
-import MyProfile from "./pages/MyProfile";
-import BookDetails from "./pages/BookDetails";
-import AddBook from "./pages/AddBook";
-import UpdateBook from "./pages/UpdateBook";
-import axios from "axios";
-import { Toaster } from "react-hot-toast";
-import ProfileEdit from "./pages/ProfileEdit";
-axios.defaults.baseURL = "http://localhost:5000";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import Prijava from './pages/Prijava';
+import Registracija from './pages/Registracija';
+import MyProfile from './pages/MyProfile';
+import BookDetails from './pages/BookDetails';
+import AddBook from './pages/AddBook';
+import UpdateBook from './pages/UpdateBook';
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
+import ProfileEdit from './pages/ProfileEdit';
+import Knjiznica from './pages/Knjiznica';
+
+axios.defaults.baseURL = 'http://localhost:5000';
 axios.defaults.withCredentials = true;
+
 const App = () => {
-  return (
-    <Router>
-      <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Route path="/" component={HomePage} exact />
-          <Route path="/prijava" component={Prijava} exact />
-          <Route path="/registracija" component={Registracija} exact />
-          <Route path="/profil" component={MyProfile} exact />
-          <Route path="/profil-edit" component={ProfileEdit} exact />
-          <Route path="/bookDetails/:id" component={BookDetails} exact />
-          <Route path="/updateBook/:id" component={UpdateBook} exact />
-          <Route path="/addBook" component={AddBook} exact />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
+	return (
+		<Router>
+			<Toaster
+				position='top-center'
+				toastOptions={{ duration: 4000 }}
+			/>
+			<Header />
+			<main>
+				<Route
+					path='/'
+					component={HomePage}
+					exact
+				/>
+				<Route
+					path='/prijava'
+					component={Prijava}
+					exact
+				/>
+				<Route
+					path='/registracija'
+					component={Registracija}
+					exact
+				/>
+				<Route
+					path='/profil'
+					component={MyProfile}
+					exact
+				/>
+				<Route
+					path='/profil-edit'
+					component={ProfileEdit}
+					exact
+				/>
+				<Route
+					path='/bookDetails/:id'
+					component={BookDetails}
+					exact
+				/>
+				<Route
+					path='/updateBook/:id'
+					component={UpdateBook}
+					exact
+				/>
+				<Route
+					path='/addBook'
+					component={AddBook}
+					exact
+				/>
+				<Route
+					path='/knjiznica'
+					component={Knjiznica}
+					exact
+				/>
+			</main>
+			<Footer />
+		</Router>
+	);
 };
 
 export default App;
--- knjiznica-marko-marulic/src/components/Header.js
+++ knjiznica-marko-marulic/src/components/Header.js
@@ -1,94 +1,99 @@
-import React from "react";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
-import { Link, Redirect } from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  function redirect() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user_id");
-    localStorage.removeItem("role");
-    localStorage.removeItem("first_name");
-    window.location.href = '/';
-  }
-  const firstName = localStorage.getItem("firstName") || undefined
+	function redirect() {
+		localStorage.removeItem('token');
+		localStorage.removeItem('id');
+		localStorage.removeItem('role');
+		localStorage.removeItem('firstName');
+		window.location.href = '/';
+	}
+	const firstName = localStorage.getItem('firstName') || undefined;
 
-
-
-  return (
-    <header>
-      <Navbar
-        expand='lg'
-        collapseOnSelect
-        style={{
-          backgroundColor: "rgb(118 119 121)",
-        }}
-      >
-        <Container>
-          <Navbar.Brand>
-            <Link
-              className='header-brand-name'
-              to='/'
-            >
-              Knjiznica Marko Marulic
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto d-flex align-items-center'>
-              <Nav.Link
-                href='/'
-                style={{
-                  color: "white",
-                }}
-              >
-                <i className='fa-solid fa-home'></i> Početna
-              </Nav.Link>
-              <Nav.Link
-                href='/prijava'
-                style={{
-                  color: "white",
-                }}
-              >
-                Prijava
-              </Nav.Link>
-              <Nav.Link
-                href='/registracija'
-                style={{
-                  color: "white",
-                }}
-              >
-                Registracija
-              </Nav.Link>
-              <Nav.Link
-                href='/profil'
-                style={{
-                  color: "white",
-                }}
-              >
-               
-                Profil 
-                
-                {firstName && (
-                <span>- {firstName}</span>
-              )}
-             
-              </Nav.Link>
-              <Button
-                onClick={redirect}
-                variant='warning'
-                size='sm'
-                style={{
-                  height: "40px",
-                }}
-              >
-                Odjava
-              </Button>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+	return (
+		<header>
+			<Navbar
+				expand='lg'
+				collapseOnSelect
+				className='navbar navbar-expand-lg bg-dark'
+				data-bs-theme='dark'
+			>
+				<Container>
+					<Navbar.Brand>
+						<Link
+							className='header-brand-name'
+							to='/'
+						>
+							Knjiznica Marko Marulic
+						</Link>
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto d-flex align-items-center'>
+							<Link
+								to='/'
+								className='text-decoration-none  mx-1'
+								style={{
+									color: 'white',
+								}}
+							>
+								<i className='fa-solid fa-home'></i> Početna
+							</Link>
+							<Link
+								to='/knjiznica'
+								className='text-decoration-none  mx-1'
+								style={{
+									color: 'white',
+								}}
+							>
+								<i class='fa-solid fa-book mx-1'></i> Knjižnica
+							</Link>
+							<Link
+								to='/profil'
+								className='text-decoration-none mx-1'
+								style={{
+									color: 'white',
+								}}
+							>
+								<i className='fa-solid fa-user mx-1'></i>Profil
+								{firstName && <span className='mx-1'> - {firstName}</span>}
+							</Link>
+							{firstName ? (
+								<Button
+									onClick={redirect}
+									variant='warning'
+									size='sm'
+									style={{
+										height: '40px',
+									}}
+								>
+									Odjava
+								</Button>
+							) : (
+								<Button
+									className=' mx-2'
+									variant='warning'
+									size='sm'
+									style={{
+										height: '40px',
+									}}
+								>
+									<Link
+										to='/prijava'
+										className='text-decoration-none'
+									>
+										Prijava
+									</Link>
+								</Button>
+							)}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
 };
 
 export default Header;
